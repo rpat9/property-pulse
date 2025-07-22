cfd7d540febc2da3fae6f1cdfbed828b11f328bb
rpat9 <rudrap26176@gmail.com>
2025-07-22 17:47:56 -0500

    Modified app.jsx for landing page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import Navbar from "./components/LandingPage/Navbar";
+import Hero from "./components/LandingPage/Hero";
+// import Metrics from "./components/LandingPage/Metrics";
+// import Featured from "./components/LandingPage/Featured";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
+  const LandingPage = () => {
+    return (
+      <div>
+        <Hero />
+        {/* <Metrics /> */}
+        {/* <Featured /> */}
+      </div>
+    )
+  }
+
   return (
-
     <Router>
-
-      <Routes>
-
-        <Route path="/" element={ <Navbar/>} />
-
-      </Routes>
+      {/* Navbar will appear on all pages */}
+      <Navbar />
       
+      {/* Main content wrapper with top padding to account for fixed navbar */}
+      <main className="pt-20">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          {/* Add more routes here as needed */}
+          {/* <Route path="/about" element={<AboutPage />} /> */}
+          {/* <Route path="/contact" element={<ContactPage />} /> */}
+        </Routes>
+      </main>
     </Router>
-
   )
 }
 
